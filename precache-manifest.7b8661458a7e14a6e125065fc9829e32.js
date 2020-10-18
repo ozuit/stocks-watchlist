bf4d92abbac90c79e972a96703c8860e366754a9
ozuit <tantd2@vng.com.vn>
2020-10-18 22:19:00 +0700

    Init

---
+++ precache-manifest.7b8661458a7e14a6e125065fc9829e32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63275f9d0bb5a5e984b05e10936614ff",
+    "url": "/stocks-watchlist/index.html"
+  },
+  {
+    "revision": "4fdc36899454a8cb23eb",
+    "url": "/stocks-watchlist/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "4fc3a2430d1a5531b7ec",
+    "url": "/stocks-watchlist/static/css/main.c7b00133.chunk.css"
+  },
+  {
+    "revision": "4fdc36899454a8cb23eb",
+    "url": "/stocks-watchlist/static/js/2.6ab18e9c.chunk.js"
+  },
+  {
+    "revision": "05235986e2a1f962e60754581aae269a",
+    "url": "/stocks-watchlist/static/js/2.6ab18e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc3a2430d1a5531b7ec",
+    "url": "/stocks-watchlist/static/js/main.3d627e3a.chunk.js"
+  },
+  {
+    "revision": "4192215e811ee29b8bde",
+    "url": "/stocks-watchlist/static/js/runtime-main.2e52cdac.js"
+  }
+]);
